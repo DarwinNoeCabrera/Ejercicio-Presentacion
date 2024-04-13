815c0fb0b2fb7327bd8818d4b6b46e6fd9a5d616
Noe_C <Noe_C@DESKTOP-Q83H4O7>
2024-04-13 10:24:46 -0600

    intentando haver los envios

---
+++ Datos/Modelo/Citad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Modelo
+{
+    public class Citad
+    {
+        //cambios
+        [Key]
+        public int CitaId { get; set; }
+        public Medicod MedicoID { get; set; }
+        public int Medicosd {get; set; }
+        public Paciented PacienteId { get; set; }
+        public int Pacientesd { get; set; }
+        public DateTime FechaCita { get; set; }
+        public bool Estado { get; set; }
+    }
+}
